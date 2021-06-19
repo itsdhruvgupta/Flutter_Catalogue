26725cdadbd88773495f58a4cc8d3409e2440d84
FlutLab.io <>
2021-06-19 14:02:47 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext ) {
     return MaterialApp(
       
         themeMode: ThemeMode.light,
