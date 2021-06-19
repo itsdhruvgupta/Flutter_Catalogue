940b16acba25903b32873808602b040cac4fd1f5
FlutLab.io <>
2021-06-19 14:25:00 +0000

    Autosave

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,13 +5,15 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       child: Center(
-        child: Text("this is login page",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.blue,
-              fontWeight: FontWeight.bold,
-            )),
-      ),
-    );
+        child: Text(
+          "this is login page",
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.blue,
+            fontWeight: FontWeight.bold,
+          ), //style
+        ), //text
+      ), //center
+    ); //matarial
   }
 }
