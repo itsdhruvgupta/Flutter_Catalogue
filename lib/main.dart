8e57973e085fddc964ec936eb1d725c483754c50
FlutLab.io <>
2021-06-19 13:50:41 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: HomePage(),
-        themeMode: ThemeMode.dark,
+        themeMode: ThemeMode.light,
         theme: ThemeData(primarySwatch: Colors.deepPurple),
         darkTheme: ThemeData(
           brightness: Brightness.dark,
