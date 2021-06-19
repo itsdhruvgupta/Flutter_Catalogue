7c1ce8d310074690f7bd373c6837720c59c79a41
FlutLab.io <>
2021-06-19 14:12:41 +0000

    Autosave

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Widget1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Center(
+      child: Text("this is login page"),
+    ));
+  }
+}
