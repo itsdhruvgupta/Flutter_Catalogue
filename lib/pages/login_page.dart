c289c1eefe639459d04038712ce3833bb3008dc5
FlutLab.io <>
2021-06-20 08:41:28 +0000

    Autosave

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -31,11 +31,12 @@ class LoginPage extends StatelessWidget {
               hintText: "Enter User Name",
               labelText: " User Name ",
             ),
-            // TextFormField(
-            // decoration: const InputDecoration(
-            //   hintText: "Enter Password",
-            //   labelText: " Password ",
-            // ),
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: "Enter Password",
+                labelText: " Password ",
+              ),
+            ),
           ),
         ], //column
       ), //childern
