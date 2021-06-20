c53aa934437e9064684ae40b1efe0d6a9751bc4e
FlutLab.io <>
2021-06-20 09:35:52 +0000

    Autosave

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,59 +4,61 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 30.0,
-          ), //box
-          Image.asset(
-            "assets/images/login.png",
-            fit: BoxFit.cover,
-          ), //children - image
-          SizedBox(
-            height: 22.0,
-          ), //box
-          Text(
-            "Welcome",
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-            ), //style
-          ), //text
-          SizedBox(
-            height: 22.0,
-          ), //box
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Enter User Name",
-                    labelText: " User Name ",
-                  ), //decoration
-                ), //textfield
-                TextFormField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    hintText: "Enter Password",
-                    labelText: " Password ",
-                  ), //decoration
-                ), //textfield
-                SizedBox(
-                  height: 22.0,
-                ), //box
-                ElevatedButton(
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(),
-                    onPressed: () {
-                      print("hii dhruv");
-                    }), //button
-              ], //children
-            ), //column
-          ), //padding
-        ], //column
-      ), //childern
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30.0,
+            ), //box
+            Image.asset(
+              "assets/images/login.png",
+              fit: BoxFit.cover,
+            ), //children - image
+            SizedBox(
+              height: 22.0,
+            ), //box
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ), //style
+            ), //text
+            SizedBox(
+              height: 22.0,
+            ), //box
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Enter User Name",
+                      labelText: " User Name ",
+                    ), //decoration
+                  ), //textfield
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: " Password ",
+                    ), //decoration
+                  ), //textfield
+                  SizedBox(
+                    height: 22.0,
+                  ), //box
+                  ElevatedButton(
+                      child: Text("Login"),
+                      style: TextButton.styleFrom(),
+                      onPressed: () {
+                        print("hii dhruv");
+                      }), //button
+                ], //children
+              ), //column
+            ), //padding
+          ], //column
+        ), //childern
+      ),//scrollview
     ); //matarial
   }
 }
