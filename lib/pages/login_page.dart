f3a22b06ad0e73199bb81bcc660daf022247e870
FlutLab.io <>
2021-06-20 09:41:40 +0000

    Autosave

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -49,7 +49,7 @@ class LoginPage extends StatelessWidget {
                   ), //box
                   ElevatedButton(
                       child: Text("Login"),
-                      style: TextButton.styleFrom(minimumSize: Size.fromWidth(100)),
+                      style: TextButton.styleFrom(minimumSize: Size.fromWidth(200)),
                       onPressed: () {
                         print("hii dhruv");
                       }), //button
