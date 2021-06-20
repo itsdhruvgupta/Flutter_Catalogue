d2bd58f1019eb5af7c3672e431360c997aa6f72c
FlutLab.io <>
2021-06-20 08:33:14 +0000

    Autosave

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,37 +4,40 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(children: [
-        SizedBox(
-          height: 22.0,
-        ), //box
-        Image.asset(
-          "assets/images/login.png",
-          fit: BoxFit.cover,
-        ), //children - image
-        SizedBox(
-          height: 22.0,
-        ), //box
-        Text(
-          "Welcome",
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-          ), //style
-        ), //text
-        SizedBox(
-          height: 22.0,
-        ), //box
-        // TextFormField(
-        //   decoration: InputDecoration(lableText: "User Name",
-        //     hintText: " Enter User Name",
-        //   ),
-        // ),
-        TextFormField(
-          
-        ),
-      ], //column
-          ), //childern
+      child: Column(
+        children: [
+          SizedBox(
+            height: 22.0,
+          ), //box
+          Image.asset(
+            "assets/images/login.png",
+            fit: BoxFit.cover,
+          ), //children - image
+          SizedBox(
+            height: 22.0,
+          ), //box
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ), //style
+          ), //text
+          SizedBox(
+            height: 22.0,
+          ), //box
+          // TextFormField(
+          //   decoration: InputDecoration(lableText: "User Name",
+          //     hintText: " Enter User Name",
+          //   ),
+          // ),
+          TextFormField(
+            DecoratedBox(
+              hintText: "Enter User Name",
+            ),
+          ),
+        ], //column
+      ), //childern
     ); //matarial
   }
 }
