81ba88a4e955c44e51e4f119e3b66e6a93157ae1
FlutLab.io <>
2021-06-21 17:04:48 +0000

    Autosave

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -24,7 +24,6 @@ class MyDrawer extends StatelessWidget {
             ListTile(
               leading: Icon(
                 CupertinoIcons.home,
-                color: Colors.white,
               ),
             ),
           ],
