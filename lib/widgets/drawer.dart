05f91b882a04a3ef36e897d32606e77a3306c4bc
FlutLab.io <>
2021-06-21 16:33:12 +0000

    Autosave

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
+  final imageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftwitter.com%2Fdhruvguptattr&psig=AOvVaw1zEeYWNReohNDzAIfMRoZg&ust=1624379513447000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCNiv5rKTqfECFQAAAAAdAAAAABAD";
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -11,6 +12,7 @@ class MyDrawer extends StatelessWidget {
             child: UserAccountsDrawerHeader(
               accountName: Text("Dhruv"),
               accountEmail: Text("dhruvg304@"),
+              currentAccountPicture: Image.network(imageUrl),
             ),
           ),
         ],
