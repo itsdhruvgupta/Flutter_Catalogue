dd13b56cf5628a4892c0529dc56ca6cce89972db
FlutLab.io <>
2021-06-21 16:54:55 +0000

    Autosave

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -24,8 +24,6 @@ class MyDrawer extends StatelessWidget {
             const Divider(
               height: 20,
               thickness: 5,
-              indent: 20,
-              endIndent: 20,
             ),
           ],
         ),
