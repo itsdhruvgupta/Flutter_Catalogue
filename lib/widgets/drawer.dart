c5638b2a92611e3e84fab28805b211f971a12b92
FlutLab.io <>
2021-06-21 16:16:29 +0000

    Autosave

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -3,6 +3,23 @@ import 'package:flutter/material.dart';
 class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: ListView(
+        childern: [
+          DrawerHeader(
+                                  decoration: BoxDecoration(
+                                      color: Colors.blue,
+                                  ),
+                                  child: Text(
+                                      'Drawer Header',
+                                      style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 24,
+                                      ),
+                                  ),
+                              ),
+        ],
+      ),
+    );
   }
 }
