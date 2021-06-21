724bfebc44ee56ce9456f3da4930ce80e872de59
FlutLab.io <>
2021-06-21 10:56:47 +0000

    Autosave

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -81,9 +81,6 @@ class _LoginPageState extends State<LoginPage> {
                         duration: Duration(
                           seconds: 1,
                         ),
-                        borderRadius: BorderRadius.circular(
-                          8,
-                        ),
                         width: changeButton ? 50 : 100,
                         height: 50,
                         alignment: Alignment.center,
