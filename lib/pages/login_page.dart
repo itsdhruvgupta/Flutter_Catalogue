b3d08fd2af698586e008744d632a2bcebdcf7216
FlutLab.io <>
2021-06-21 10:41:09 +0000

    Autosave

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -73,6 +73,9 @@ class _LoginPageState extends State<LoginPage> {
                         });
                         await Future.delayed(Duration(seconds: 1));
                         Navigator.pushNamed(context, MyRoutes.homeRoute);
+                        setState(() {
+                          changeButton = false;
+                        });
                       },
                       child: AnimatedContainer(
                         duration: Duration(
