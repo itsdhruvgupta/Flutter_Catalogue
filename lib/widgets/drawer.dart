31548dee91efc134ff5b7c73ebb5bce60cb2118f
FlutLab.io <>
2021-06-21 16:25:20 +0000

    Autosave

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -4,6 +4,7 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      padding: EdgeInsets.all(0),
       child: ListView(
         children: [
           DrawerHeader(
