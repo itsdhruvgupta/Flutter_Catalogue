a9e7421c10175adad0ee610b1d06ef18d1eeadf4
FlutLab.io <>
2021-06-21 16:54:21 +0000

    Autosave

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -21,6 +21,12 @@ class MyDrawer extends StatelessWidget {
                 ),
               ),
             ),
+            const Divider(
+              height: 20,
+              thickness: 5,
+              indent: 20,
+              endIndent: 20,
+            ),
           ],
         ),
         color: Colors.deepPurple,
