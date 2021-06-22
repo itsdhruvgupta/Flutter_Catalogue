2dbe9382d91f45cd107909055ba4232dbd2e097c
FlutLab.io <>
2021-06-22 06:17:42 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'pages/home_page.dart';
 import 'pages/login_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'utils/routes.dart';
+import 'widgets/themes.dart';
 
 // just to try the git and vcs
 void main() {
@@ -13,14 +14,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      themeMode: Thememode.light,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
---
+++ lib/widgets/themes.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Mytheme {
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamaly: GoogleFonts.lato().fontFamily,
+        appBar: AppBarTheme(
+          color: Colors.white,
+          iconTheme: IconThemeData(color: Colors.black),
+          textTheme: Theme.of(context).textTheme,
+        ),
+      );
+
+  static ThemeData darkTheme(BuildContext context) => ThemeData(
+        brightness: Brightness.dark,
+      );
+}
