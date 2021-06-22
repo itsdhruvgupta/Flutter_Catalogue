2a0e2441d017f3566361199edc3a9a312273ea1f
FlutLab.io <>
2021-06-22 06:21:59 +0000

    Autosave

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Mytheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
         primarySwatch: Colors.deepPurple,
-        fontFamaly: GoogleFonts.lato().fontFamily,
+        fontFamily: GoogleFonts.lato().fontFamily,
         appBar: AppBarTheme(
           color: Colors.white,
           iconTheme: IconThemeData(color: Colors.black),
