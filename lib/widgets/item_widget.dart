a8e65e9b174c366b79671756215b62ee625c2438
FlutLab.io <>
2021-06-25 10:09:15 +0000

    Autosave

--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -15,7 +15,14 @@ class ItemWidget extends StatelessWidget {
         leading: Image.network(item.image),
         title: Text(item.name),
         subtitle: Text(item.desc),
-        trailing: Text("\$ ${item.price}"),
+        trailing: Text(
+          "\$ ${item.price}",
+          textScaleFactor: 1.2,
+          style: TextStyle(
+            color: Colors.deepPurple,
+            fontWeight: Fontweight.bold,
+          ),
+        ),
       ),
     );
   }
