a2a3abfe24cbc04c4ff792f0ca9e877a02c448a8
FlutLab.io <>
2021-06-25 15:50:04 +0000

    Autosave

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -10,33 +10,6 @@ class Item {
   final String color;
   final String image;
 
-  Item({
-    required this.id,
-    required this.name,
-    required this.desc,
-    required this.price,
-    required this.color,
-    required this.image,
-  });
-
-  Item copyWith({
-    int? id,
-    String? name,
-    String? desc,
-    num? price,
-    String? color,
-    String? image,
-  }) {
-    return Item(
-      id: id ?? this.id,
-      name: name ?? this.name,
-      desc: desc ?? this.desc,
-      price: price ?? this.price,
-      color: color ?? this.color,
-      image: image ?? this.image,
-    );
-  }
-
   Map<String, dynamic> toMap() {
     return {
       'id': id,
@@ -67,29 +40,4 @@ class Item {
   String toString() {
     return 'Item(id: $id, name: $name, desc: $desc, price: $price, color: $color, image: $image)';
   }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is Item && other.id == id && other.name == name && other.desc == desc && other.price == price && other.color == color && other.image == image;
-  }
-
-  @override
-  int get hashCode {
-    return id.hashCode ^ name.hashCode ^ desc.hashCode ^ price.hashCode ^ color.hashCode ^ image.hashCode;
-  }
-}
-
-class CatalogModel {
-  static final items = [
-    Item(
-      id: 1,
-      name: " iphone",
-      price: 21000,
-      color: "33505a",
-      desc: "Iphone 12 pro max sale",
-      image: "assets/images/iphone.jpg",
-    )
-  ];
 }
