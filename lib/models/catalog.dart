5ba158fd60c28ae5231728c93f0c527f4dec00a4
FlutLab.io <>
2021-06-25 15:51:01 +0000

    Autosave

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 
 class Item {
@@ -9,6 +7,21 @@ class Item {
   final num price;
   final String color;
   final String image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+
+class CatalogModel {
+  static final items = [
+    Item(
+      id: 1,
+      name: " iphone",
+      price: 21000,
+      color: "33505a",
+      desc: "Iphone 12 pro max sale",
+      image: "assets/images/iphone.jpg",
+    )
+  ];
 
   Map<String, dynamic> toMap() {
     return {
@@ -21,23 +34,5 @@ class Item {
     };
   }
 
-  factory Item.fromMap(Map<String, dynamic> map) {
-    return Item(
-      id: map['id'],
-      name: map['name'],
-      desc: map['desc'],
-      price: map['price'],
-      color: map['color'],
-      image: map['image'],
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory Item.fromJson(String source) => Item.fromMap(json.decode(source));
-
-  @override
-  String toString() {
-    return 'Item(id: $id, name: $name, desc: $desc, price: $price, color: $color, image: $image)';
-  }
+  
 }
