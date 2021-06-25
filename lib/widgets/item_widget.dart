f097137826a8f7f3b609708039b2bcfd8a676a44
FlutLab.io <>
2021-06-25 10:12:26 +0000

    Autosave

--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -11,12 +11,10 @@ class ItemWidget extends StatelessWidget {
 
   Widget build(BuildContext context) {
     return Card(
+      elevation: 0.0,
       child: ListTile(
         leading: Image.network(item.image),
-        title: Text(
-          item.name,
-          textScaleFactor: 1.2,
-        ),
+        title: Text(item.name),
         subtitle: Text(item.desc),
         trailing: Text(
           "\$ ${item.price}",
