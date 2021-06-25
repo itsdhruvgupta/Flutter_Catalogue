d1be70affcfb7e5677f1bc19b01481caaa640d7e
FlutLab.io <>
2021-06-25 15:54:42 +0000

    Autosave

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -10,17 +10,17 @@ class Item {
 
   Item({this.id, this.name, this.desc, this.price, this.color, this.image});
 
-  fromMap() {
-    returnMap<String, dynamic> toMap() {
-      return {
-        'id': id,
-        'name': name,
-        'desc': desc,
-        'price': price,
-        'color': color,
-        'image': image,
-      };
-    }
+  factory Item.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return Item(
+      id: map['id'],
+      name: map['name'],
+      desc: map['desc'],
+      price: map['price'],
+      color: map['color'],
+      image: map['image'],
+    );
   }
 }
 
